a558f10685a76d2bb164947802d5224b9a8eab91
Brozekm <56959582+Brozekm@users.noreply.github.com>
2020-02-14 17:28:27 +0100

    Add files via upload

---
+++ Controllers/IntroductionController.class.php
@@ -0,0 +1,33 @@
+<?php
+// nactu rozhrani kontroleru
+require_once(DIRECTORY_CONTROLLERS."/IController.interface.php");
+
+/**
+ * Ovladac zajistujici vypsani uvodni stranky.
+ */
+class IntroductionController implements IController {
+
+    /**
+     * Vrati obsah uvodni stranky.
+     * @param string $pageTitle     Nazev stranky.
+     * @return string               Vypis v sablone.
+     */
+    public function show(string $pageTitle):string {
+        //// vsechna data sablony budou globalni
+        global $tplData;
+        $tplData = [];
+        // nazev
+        $tplData['title'] = $pageTitle;
+        ob_start();
+        // pripojim sablonu, cimz ji i vykonam
+        require(DIRECTORY_VIEWS ."/Introduction.tpl.php");
+        // ziskam obsah output bufferu, tj. vypsanou sablonu
+        $obsah = ob_get_clean();
+
+        // vratim sablonu naplnenou daty
+        return $obsah;
+    }
+
+}
+
+?>
